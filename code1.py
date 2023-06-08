643dd6f285f1ad60fb2ee6a3582d9ec0d419c301
XxGonanxX <patomorelia@hotmail.com>
2023-06-08 17:49:51 -0600

    First two codes

---
+++ code1.py
@@ -0,0 +1,32 @@
+#==============================================================================
+#
+#Created by: Alan Patricio González Bernal
+#Date: 2023-06-08
+#language: Python
+#
+#==============================================================================
+
+#INSTRUCTIONS
+
+#==============================================================================
+# Create a program that asks the user for a number and then prints out a 
+# list of all the divisors of that number.
+# (If you don’t know what a divisor is, it is a number that divides evenly)
+#==============================================================================
+
+#Variable definition
+selected = int(input("Enter a number: "))
+divisors = []
+chosen = selected
+
+#Method
+while selected > 0:
+    if selected % 2 == 0:
+        divisors.append(selected)
+    selected -= 1
+    
+#Final prints
+print("The divisors of:")
+print(chosen)
+print("are:")
+print(divisors)
---
+++ code2.py
@@ -0,0 +1,32 @@
+#==============================================================================
+#
+#Created by: Alan Patricio González Bernal
+#Date: 2023-06-08
+#language: Python
+#
+#==============================================================================
+
+#INSTRUCTIONS
+
+#==============================================================================
+# Create a simple calculator that asks the user for two numbers and an operator
+# (+, -, *, /). Then calculate the result and print it out to the user.
+#==============================================================================
+
+#We define the variables
+n1 = int(input("Enter a number: "))
+n2 = int(input("Enter another number: "))
+operator = input("Enter an operator (+, -, *, /): ")
+
+print("The result of your selection is: ")
+
+#Method of operation
+if operator == "+":
+    print(n1 + n2)
+elif operator == "-":
+    print(n1 - n2)
+elif operator == "*":
+    print(n1 * n2)
+elif operator == "/":
+    print(n1 / n2)
+    
